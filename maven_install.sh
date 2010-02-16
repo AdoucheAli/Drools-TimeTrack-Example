374d4f83621aa2200b9374491f77947ea027b991
mdarrett <mdarrett@mdarrett.fedora>
2010-02-16 13:06:01 -0800

    (1) Create a maven build environment (2) Create a run.sh to execute the maven built timekeeper-1.0.0 jar file

---
+++ maven_install.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=com.jboss.brms -DartifactId=brms-api -Dversion=5.0.1 -Dpackaging=jar -Dfile=./lib/drools-api-5.0.1.BRMS.jar 
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp target/timekeeper-1.0.0.jar:lib/antlr-runtime-3.1.1.jar:lib/drools-api-5.0.1.BRMS.jar:lib/drools-compiler-5.0.1.BRMS.jar:lib/drools-core-5.0.1.BRMS.jar:lib/mvel2-2.0.10.jar:lib/org.eclipse.jdt.core_3.4.4.v_894_R34x.jar:lib/xstream-1.3.1.jar com.jboss.example.DroolsTest
